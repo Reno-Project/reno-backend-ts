e07b089a80f22e91a8df22426974c9b047948742
MilanJulinac42 <milanjulinac996@gmail.com>
2026-09-16 09:28:17 +0200

    fix: remove invalid cors credentials with wildcard origin

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,7 @@ const app = express();
 app.use(cors({
   origin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS', 'PATCH'],
-  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'language', 'lean-signature', 'X-Reno-App-Version'],
-  credentials: true
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization', 'language', 'lean-signature', 'X-Reno-App-Version']
 }));
 
 app.use("/webhooks/lean", express.raw({ type: "*/*" }), leanWebhookRouter);
